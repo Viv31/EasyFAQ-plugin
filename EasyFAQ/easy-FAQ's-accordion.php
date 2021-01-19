e55e1add20fc25195d575d5ace000eab7e8e95cd
viv31 <vivgangs@gmail.com>
2021-01-19 18:08:16 +0530

    Add custom css/js

--- EasyFAQ/easy-FAQ's-accordion.php
+++ EasyFAQ/easy-FAQ's-accordion.php
@@ -8,72 +8,54 @@
 * Author URI:
 */
 
-?>
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<style type="text/css">
-	.card-header{
-		background-color: #007bff;
-		color:white;
-	}
-
-	.card{
-	box-shadow: 1px 1px 10px 2px #888888; 
-	margin-bottom: 35px;
-	
-	cursor: pointer;
-	border-radius: 0px!important; /* For removing bydefault rounded border */
-	width: auto;
-	}
-
-
-	/* For making minus and plus symbol by css  */
- .collapsed i.fa.fa-plus:before{
-	 content: "\2b";
-	 font-weight: bold;
-	  font-weight: bold;
-
-      
+if(!defined('ABSPATH')){
+	 exit;
 }
-/* if minus sign occure  */
- i.fa.fa-plus:before{
-	 content: "\2212";
-	font-weight: bold;
-	  font-weight: bold;
-	  float: right;
-	   
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+//adding jquery 
+function faq_load_admin_js() {
+    wp_enqueue_script( array( 'jquery' ) );
 }
-i.fa.fa-plus{
-	float: right!important;
+add_action( 'wp_enqueue_scripts', 'faq_load_admin_js' );
+add_action('init', 'register_script');
+
+function register_script() {
+    
+    wp_register_script( 'bootstrap_js', plugins_url('assets/js/bootstrap.min.js', __FILE__), array('jquery'), '2.5.1' );
+   wp_register_style( 'boostrap_css', plugins_url('assets/css/bootstrap-min.css', __FILE__), false, '1.0.0', 'all');
+   wp_register_style( 'custom_css', plugins_url('assets/css/style.css', __FILE__), false, '1.0.0', 'all');
 }
-</style>
-<?php
-function EFAQ_section($attr){
+
+// use the registered jquery and style above
+add_action('wp_enqueue_scripts', 'enqueue_style');
+
+function enqueue_style(){
+   
+   wp_enqueue_script('bootstrap_js');
+	wp_enqueue_style( 'boostrap_css' );
+   wp_enqueue_style( 'custom_css' );
+}
+
+//shortcode function
+function EFAQ_section($attr){ 
+
 	static $i=0; //checking how many times shortcode paste.
 	$i++;
 	//echo $i;
-
 	$options = shortcode_atts(array(
 	'question'=> 'question goes here?',
 	'answer'=>'answer goews here'	
 	),$attr);
 
-$output_data ="<div id='accordion'><div class='card'><div class='card-header' id='headingOne' class='btn btn-primary' data-toggle='collapse' data-target='#collapseOne".$i."' aria-expanded='true' aria-controls='collapseOne'> $i)&nbsp;".$options['question']."<i class='fa fa-plus'></i></div>
-<div id='collapseOne".$i."' class='collapse show' aria-labelledby='headingOne' data-parent='#accordion'>
+$output_data ="<div id='accordion".$i."'><div class='card'><div class='card-header' id='headingOne' class='btn btn-primary' data-toggle='collapse' data-target='#collapseOne".$i."' aria-expanded='true' aria-controls='collapseOne'> $i)&nbsp;".$options['question']."<i class='fa fa-plus'></i></div>
+<div id='collapseOne".$i."' class='collapse show' aria-labelledby='headingOne' data-parent='#accordion".$i."'>
       <div class='card-body'>
          ".$options['answer']."
       </div>
     </div>
   </div>";
-
-
 return $output_data;
-
-
 }
 
 add_shortcode('EFAQ','EFAQ_section');
